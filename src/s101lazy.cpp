7cb1a33f6d432e37eb4464e9eed63e4609dd480c
Denis Kalinin <kdv@shipbuilders.ru>
2025-10-15 16:28:30 +0300

    init

---
+++ src/s101lazy.cpp
@@ -0,0 +1,67 @@
+#include "ogrsf_frmts.h"
+#include "cpl_conv.h"
+#include "S101GML.h"
+
+class OGRS101LazyDriver: public GDALDriver{
+    public:
+        inline static const char *NAME = "S101Lazy";
+        OGRS101LazyDriver(){
+            SetDescription(NAME);
+            SetMetadataItem(GDAL_DCAP_VECTOR, "YES");
+            SetMetadataItem(GDAL_DMD_LONGNAME, "IHO S-101 lazy driver (using gml)");
+            SetMetadataItem(GDAL_DMD_EXTENSION, "000");
+            SetMetadataItem(GDAL_DCAP_VIRTUALIO, "YES");
+
+            pfnOpen = Open;
+            pfnIdentify = Identify;
+            pfnCreate = nullptr; // READ ONLY
+            
+        }
+
+        static GDALDataset *Open(GDALOpenInfo* poOpenInfo){
+            if(poOpenInfo->eAccess == GA_Update){
+                return nullptr; // READ ONLY
+            }
+            
+            libS101::S101 cell;
+            cell.Open(poOpenInfo->pszFilename);
+            std::string gmlData = S101GML::ExportToGML(cell);
+
+            const char *tempGMLPath = "/vsimem/temp.gml";
+            VSILFILE* fp = VSIFOpenL(tempGMLPath,"w");
+            if(fp){
+                VSIFWriteL(gmlData.c_str(),gmlData.size(),1,fp);
+                VSIFCloseL(fp);
+            }
+
+            GDALDriver *gmlDriver = GetGDALDriverManager()->GetDriverByName("GML");
+            if(gmlDriver && gmlDriver->pfnOpen){
+                GDALOpenInfo oOpenInfo(tempGMLPath,GA_ReadOnly,nullptr);
+                return gmlDriver->pfnOpen(&oOpenInfo);
+            }
+
+            return nullptr;
+        }
+
+        static int Identify(GDALOpenInfo* poOpenInfo){
+            const char* pszFilename = poOpenInfo->pszFilename;
+            if (EQUAL(CPLGetExtension(pszFilename), "000")) {
+                return TRUE;
+            }
+
+            return FALSE;
+        }
+
+        static void Register(){
+            if(GDALGetDriverByName(NAME)!=NULL)
+                return;
+            GDALDriver *poDriver = new OGRS101LazyDriver();
+            GetGDALDriverManager()->RegisterDriver(poDriver);
+        }
+};
+
+// Export the function properly
+extern "C" void GDALRegisterMe()
+{
+    OGRS101LazyDriver::Register();
+}
