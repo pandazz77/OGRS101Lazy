8bd11ce17803c0fac2448b46f86467331720905a
pandazz77 <kalin1538inn@gmail.com>
2025-10-15 22:15:49 +0300

    add caching

--- src/s101lazy.cpp
+++ src/s101lazy.cpp
@@ -2,12 +2,16 @@
 #include "cpl_conv.h"
 #include "S101GML.h"
 #include <mutex>
+#include <filesystem>
 
-std::mutex mtx;
 
 class OGRS101LazyDriver: public GDALDriver{
     public:
         inline static const char *NAME = "S101Lazy";
+        inline static std::string cache_data;
+        inline static std::time_t cache_time;
+        inline static std::mutex mtx;
+
         OGRS101LazyDriver(){
             SetDescription(NAME);
             SetMetadataItem(GDAL_DCAP_VECTOR, "YES");
@@ -25,17 +29,26 @@ class OGRS101LazyDriver: public GDALDriver{
             if(poOpenInfo->eAccess == GA_Update){
                 return nullptr; // READ ONLY
             }
-            
+
             mtx.lock();
-            libS101::S101 cell;
-            cell.Open(poOpenInfo->pszFilename);
-            std::string gmlData = S101GML::ExportToGML(cell);
+            auto mod_time = std::filesystem::last_write_time(poOpenInfo->pszFilename);
+            auto sctp = std::chrono::time_point_cast<std::chrono::system_clock::duration>(
+                mod_time - std::filesystem::file_time_type::clock::now() + std::chrono::system_clock::now()
+            );
+            std::time_t mod_time_t = std::chrono::system_clock::to_time_t(sctp);
+
+            if(mod_time_t!=cache_time){
+                libS101::S101 cell;
+                cell.Open(poOpenInfo->pszFilename);
+                cache_data = S101GML::ExportToGML(cell);
+                cache_time = mod_time_t;
+            }
             mtx.unlock();
 
             const char *tempGMLPath = "/vsimem/temp.gml";
             VSILFILE* fp = VSIFOpenL(tempGMLPath,"w");
             if(fp){
-                VSIFWriteL(gmlData.c_str(),gmlData.size(),1,fp);
+                VSIFWriteL(cache_data.c_str(),cache_data.size(),1,fp);
                 VSIFCloseL(fp);
             }
 
